de24a209d8ab1a894d55ff8951d088e91460dd98
Eric Searcy <emsearcy@gmail.com>
2010-09-07 11:23:46 -0700

    Add selective tab refreshing

--- content/tabslideshow.js
+++ content/tabslideshow.js
@@ -23,13 +23,37 @@ var tabslideshow = {
     {
         // grab services
         this.prefs = Components.classes['@mozilla.org/preferences-service;1']
-            .getService(Components.interfaces.nsIPrefService)
-            .getBranch('tabslideshow.');
+                .getService(Components.interfaces.nsIPrefService)
+                .getBranch('tabslideshow.');
         this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
         this.prefs.addObserver('', this, false);
 
+        // save prefs
         this.time = this.prefs.getIntPref('time');
         this.refresh = this.prefs.getBoolPref('refresh');
+
+        // add tab context menu entries and hook
+        var tabmenu = tabslideshow_gettabcontextmenu();
+        tabmenu.insertBefore(document.getElementById('tabslideshow-refresh'),
+                document.getElementById('context_closeTab'));
+        tabmenu.insertBefore(document.getElementById('tabslideshow-separator'),
+                document.getElementById('context_closeTab'));
+        tabmenu.addEventListener('popupshowing', tabslideshow.tabmenushow,
+                false);
+        // remove now-empty menupopup container
+        var popup = document.getElementById("tabslideshowPopup");
+        popup.parentNode.removeChild(popup);
+
+        // initial tabs refresh settings
+        for ( var tab in gBrowser.tabContainer.childNodes ) {
+            gBrowser.tabContainer.childNodes[tab].tabslideshowrefresh =
+                    this.refresh;
+        }
+
+        // add listener for new tabs
+        gBrowser.tabContainer.addEventListener('TabOpen', function(e) {
+                tabslideshow.checknewtab(e);
+            }, false);
 
         // initial fullscreen
         if (this.prefs.getBoolPref('start')) {
@@ -58,7 +82,8 @@ var tabslideshow = {
         gBrowser.selectedTab = nexttab;
 
         // optionally refresh upcoming tab
-        if (p_this.refresh && (nexttab != upcomingtab)) {
+        if (p_this.refresh && upcomingtab.tabslideshowrefresh &&
+                (nexttab != upcomingtab)) {
             gBrowser.reloadTab(upcomingtab);
         }
 
@@ -79,6 +104,50 @@ var tabslideshow = {
                 this.refresh = this.prefs.getBoolPref('refresh');
                 break;
         }
+    },
+
+    // handle new tab refresh settings
+    checknewtab: function(e)
+    {
+        Components.utils.reportError("checknewtab")
+        e.target.tabslideshowrefresh = this.refresh;
+    },
+
+    // per-tab menu adjustments
+    tabmenushow: function(e)
+    {
+        Components.utils.reportError('tabmenushow');
+
+        if (e.originalTarget != gBrowser.tabContextMenu) {
+            Components.utils.reportError("branch A")
+            return true;
+        }
+
+        var item;
+        var refresh = document.getElementById('tabslideshow-refresh');
+        if (document.popupNode.parentNode) {
+            Components.utils.reportError("branch B")
+            item = document.popupNode.parentNode.parentNode.id;
+        }
+        // XXX: copied from tabmixplus, not sure what does
+        if (item && (item == "btn_tabslist" || item == "btn_tabslistSorted" ||
+                item == "alltabs-button")) {
+            Components.utils.reportError("branch C")
+            gBrowser.mContextTab = document.popupNode.tab;
+        }
+
+        var clickOutTabs = document.popupNode.localName == "tabs";
+        var aTab = clickOutTabs ? gBrowser.mCurrentTab : gBrowser.mContextTab;
+           
+        if (clickOutTabs) refresh.disabled = true;
+        else refresh.disabled = false;
+
+        if (aTab.tabslideshowrefresh) {
+            refresh.setAttribute("checked", "true");
+        } else {
+            refresh.removeAttribute("checked");
+        }
+
     },
 
     // toggle whether slideshowing
@@ -103,11 +172,40 @@ var tabslideshow = {
         }
     },
 
+    // change refresh flag on tab
+    togglerefresh: function(e)
+    {
+        Components.utils.reportError('togglerefresh');
+        e.tabslideshowrefresh = !e.tabslideshowrefresh;
+    },
+
     // clean up
     shutdown: function()
     {
         this.prefs.removeObserver("", this);
+    },
+
+}
+
+// return tab context menu
+function tabslideshow_gettabcontextmenu()
+{
+    if (gBrowser.tabContextMenu) return gBrowser.tabContextMenu;
+
+    var tabmenu = document.getAnonymousElementByAttribute(
+            gBrowser, "anonid", "tabContextMenu");
+    if (tabmenu) {
+        gBrowser.tabContextMenu = tabmenu;
+    } else if (gBrowser.tabContainer && gBrowser.tabContainer.contextMenu) {
+        gBrowser.tabContextMenu = gBrowser.tabContainer.contextMenu;
+    } else {
+        gBrowser.tabContextMenu = document.getElementById('tabContextMenu');
     }
+
+    if (!gBrowser.tabContextMenu.hasAttribute("id"))
+        gBrowser.tabContextMenu.setAttribute("id", "tabContextMenu");
+
+    return gBrowser.tabContextMenu;
 }
 
 window.addEventListener('load', function(e) { tabslideshow.startup(); },
